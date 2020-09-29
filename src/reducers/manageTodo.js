4813a90d77937e44b4da32515f9d312ba9928f16
Jas802 <jas.spencer414@gmail.com>
2020-09-29 10:58:16 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,17 @@
+import { bindActionCreators } from "redux";
+
 export default function manageTodo(state = {
-  todos: [],
+  todos: [
+    { text: 'buy groceries' },
+    { text: 'watch netflix' },
+  ],
 }, action) {
-
-  console.log("reducer received this action:", action);
-
+  switch (action.type) {
+    case 'ADD_TODO':
+  
+    console.log({ tofos: state.todos.concat(action.payload.text) });
+    return { todos: state.todos.concat(action.payload.text) };
+    default:
   return state;
+  }
 }
